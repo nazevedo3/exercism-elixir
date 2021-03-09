61bb63e42e37e695e12324f34ea7b775f3f83a6c
nazevedo3 <34354453+nazevedo3@users.noreply.github.com>
2021-03-09 09:54:27 -0800

    updated bob solution

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,42 +1,32 @@
 defmodule Bob do
+  import String, only: [trim: 1]
+
   def hey(input) do
     cond do
-      is_shout_question?(input) -> "Calm down, I know what I'm doing!"
-      is_shout?(input) -> "Whoa, chill out!"
-      is_question?(input) -> "Sure."
-      is_empty?(input) -> "Fine.  Be that way!"
+      shout_question?(input) -> "Calm down, I know what I'm doing!"
+      shout?(input) -> "Whoa, chill out!"
+      question?(input) -> "Sure."
+      empty?(input) -> "Fine. Be that way!"
       true -> "Whatever."
     end
   end
 
-
-  def is_question?(input) do
+  defp question?(input) do
     trimmed = trim(input)
-    case String.ends_with?(trimmed, "?") do
-      true -> true
-      false -> false
-    end
+    String.ends_with?(trimmed, "?")
   end
 
-  def is_shout_question?(input) do
-    is_question?(input) && is_shout?(input)
+  defp shout_question?(input) do
+    question?(input) && shout?(input)
   end
 
-  def is_shout?(input) do
+  defp shout?(input) do
     String.upcase(input) == input && String.downcase(input) != input
   end
 
 
-  def is_empty?(input) do
+  defp empty?(input) do
     trimmed = trim(input)
-    cond do
-      String.length(trimmed) <= 1 -> true
-      true -> false
-    end
-  end
-
-  defp trim(input) do
-    input
-    |> String.trim
+    String.length(trimmed) <= 1
   end
 end
